c12ce1832e782996ce96f117ec08bb11136c3ffe
Sch3lp <sch3lp@gmail.com>
2016-04-04 19:47:07 +0200

    Exercise 3.2

---
+++ exercises/src/main/scala/ScalaOutput.sc
@@ -0,0 +1,3 @@
+val someList = fpinscala.datastructures.List(1,3,4,8)
+val result = fpinscala.datastructures.List.tail(someList)
+println(result)
--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,7 +50,11 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil //or throw some exception I guess?
+      case Cons(h,t) => t
+    }
 
   def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
 
