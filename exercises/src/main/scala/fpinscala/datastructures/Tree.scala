9ecdb0204e7b64257010d7805f04317477f52df5
Sch3lp <sch3lp@gmail.com>
2016-05-02 23:21:59 +0200

    fold prep; I looked at the solutions and they looked differently

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -15,7 +15,8 @@ object Tree {
     tree match {
       case Leaf(_) => 1
       //TODO Question: why does the solution have 1 + size of left and right?
-      case Branch(left, right) => size(left) + size(right)
+      //TODO Question: is it because to count all nodes you have to count "yourself" too? With yourself being a Branch of two other Trees?
+      case Branch(l, r) => 1 + size(l) + size(r)
     }
 
   def maximum(t: Tree[Int]): Int =
@@ -24,16 +25,19 @@ object Tree {
       case Branch(l, r) => maximum(l) max maximum(r)
     }
 
+  //TODO Question: they return 0 as Leaf because the "root" counts as depth 0?
   def depth[A](t: Tree[A]): Int =
     t match {
-      case Leaf(_) => 1
-      case Branch(l, r) => size(l) max size(r)
+      case Leaf(_) => 0
+      case Branch(l, r) => 1 + (depth(l) max depth(r))
     }
 
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
     t match {
       case Leaf(a) => Leaf(f(a))
-      case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
     }
+
+//  def fold
 }
 
