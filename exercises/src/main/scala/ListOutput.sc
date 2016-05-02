0606bee1c063b8e7dd70383460d127f60bb61e5a
Sch3lp <sch3lp@gmail.com>
2016-05-02 23:14:07 +0200

    Exercise 3.28 map

--- exercises/src/main/scala/ListOutput.sc
+++ exercises/src/main/scala/ListOutput.sc
@@ -19,26 +19,3 @@ List.reverse(List(4,3,2,1))
 List.reverse(List("A","B","C"))
 List.map(List(1, 2, 3, 4))((a) => 42)
 List.reverse(List(1,2,3,4)) == List(4,3,2,1)
-Leaf(1)
-def tree1 = Leaf(1)
-def tree2 = Branch(tree1,Leaf(2))
-def tree3 = Branch(Leaf(1),Branch(Leaf(2),Leaf(3)))
-def tree32 = Branch(Branch(Leaf(2),Leaf(3)),tree1)
-
-val leftBranch = tree2
-val rightBranch = Branch(Leaf(5),Branch(Leaf(4),Leaf(3)))
-
-def tree5 = Branch(leftBranch,rightBranch)
-Tree.size(tree1)
-Tree.size(tree2)
-Tree.size(tree3)
-Tree.size(tree32)
-Tree.size(tree5)
-
-Tree.maximum(tree5)
-
-Tree.depth(tree1)
-Tree.depth(tree2)
-Tree.depth(tree3)
-Tree.depth(tree32)
-Tree.depth(tree5)
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -3,26 +3,37 @@ package fpinscala.datastructures
 import javax.print.DocFlavor.BYTE_ARRAY
 
 sealed trait Tree[+A]
+
 case class Leaf[A](value: A) extends Tree[A]
+
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
 
-  def size[A](tree: Tree[A]): Int = tree match {
-    case Leaf(_) => 1
-    //TODO Question: why does the solution have 1 + size of left and right?
-    case Branch(left, right) => size(left) + size(right)
-  }
+  def size[A](tree: Tree[A]): Int =
+    tree match {
+      case Leaf(_) => 1
+      //TODO Question: why does the solution have 1 + size of left and right?
+      case Branch(left, right) => size(left) + size(right)
+    }
 
-  def maximum(t: Tree[Int]): Int = t match {
-    case Leaf(a) => a
-    case Branch(l,r) => maximum(l) max maximum(r)
-  }
+  def maximum(t: Tree[Int]): Int =
+    t match {
+      case Leaf(a) => a
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
 
-  def depth[A](t: Tree[A]): Int = t match {
-    case Leaf(_) => 1
-    case Branch(l,r) => size(l) max size(r)
-  }
+  def depth[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => size(l) max size(r)
+    }
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    t match {
+      case Leaf(a) => Leaf(f(a))
+      case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+    }
 }
 
